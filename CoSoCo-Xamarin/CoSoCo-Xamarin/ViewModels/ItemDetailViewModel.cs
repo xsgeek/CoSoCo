63931e0fba1f13a40098b1afac3fc4c95eb2d120
XsGeek <mh@xsgeek.com>
2019-01-01 15:56:06 -0500

    Initial Commit of Hello World Xamarin Projects

---
+++ CoSoCo-Xamarin/CoSoCo-Xamarin/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using CoSoCo_Xamarin.Models;
+
+namespace CoSoCo_Xamarin.ViewModels
+{
+	public class ItemDetailViewModel : BaseViewModel
+	{
+		public Item Item { get; set; }
+		public ItemDetailViewModel(Item item = null)
+		{
+			Title = item?.Text;
+			Item = item;
+		}
+	}
+}
